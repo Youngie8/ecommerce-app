57d081bae1077a77ad7cee9f9e0f6f3cc59c3bf5
Oluwole-young Taiwo <taiwooluwoleyoung@gmail.com>
2024-12-15 09:51:03 +0100

    worked on the search bar

--- src/home/Banner.jsx
+++ src/home/Banner.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import productData from "../products.json"
+import { Link } from 'react-router-dom';
 const title = (
     <h2>Get your Product without Worrying About <span>Financal</span> Mess!</h2>
 );
-const desc = "We have the largest collection of product";
+const desc = "We have the largest collection of products!";
 const bannerList = [
     {
     iconName: "icofont-users-alt-4",
@@ -19,8 +20,18 @@ const bannerList = [
     },
     ];
 const Banner = () => {
-      const [searchInput, setSearchInput] = useState('');
-      const [filterProducts, setFilterProducts] = useState(productData);
+    const [searchInput, setSearchInput] = useState('');
+    const [filterProducts, setFilterProducts] = useState(productData);
+
+    // Filtering func
+    const handleSearch = e => {
+        const searchTerm = e.target.value
+        setSearchInput(searchTerm)
+        // Filtering products based on search
+        const filtered = productData.filter((product) => product.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+        setFilterProducts(filtered)
+    }
 
   return (
     <div className='banner-section style-4'>
@@ -28,8 +39,16 @@ const Banner = () => {
             <div className="banner-content">
                 {title}
                 <form>
-                    <input type="text" name="search" id="search" placeholder='search your product'/>
+                    <input type="text" name="search" id="search" placeholder='search your product' value={searchInput} onChange={handleSearch}/>
                 </form>
+                <p>{desc}</p>
+                <ul className="lab-ul">
+                    {
+                        searchInput && filterProducts.map((product, i) => <li key={i}>
+                            <Link to={`/shop/${product.id}`}>{product.name}</Link>
+                        </li>)
+                    }
+                </ul>
             </div>
         </div>
     </div>
